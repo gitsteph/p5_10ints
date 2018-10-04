3ac1f0bcbd26b3a29da570d0590f6d8b24c15b8d
Stephanie H. Andrews <byronic.string@gmail.com>
2018-10-03 23:30:18 -0700

    initial commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    var canvasWidth = windowWidth * 0.95;
+    var canvasHeight = windowHeight * 0.95;
+    coreSketch = select('#coreSketch');
+    var canvas = createCanvas(canvasWidth, canvasHeight);
+    canvas.parent(coreSketch);
+    cursor(CROSS);
+
+}
+
+function draw() {
+
+}
+
+function mousePressed() {
+}
